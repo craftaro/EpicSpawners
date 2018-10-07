ec901f5b3dabc16eb5dd41d67dc0378803b849f0
The_Creeper_Cow <lars.dormans@live.nl>
2018-10-07 16:14:35 +0200

    Fixes #SD-51

--- EpicSpawners-Plugin/src/main/java/com/songoda/epicspawners/spawners/spawner/ESpawner.java
+++ EpicSpawners-Plugin/src/main/java/com/songoda/epicspawners/spawners/spawner/ESpawner.java
@@ -576,7 +576,7 @@ public class ESpawner implements Spawner {
                     && player.getItemInHand() != null
                     && player.getItemInHand().hasItemMeta()
                     && player.getItemInHand().getItemMeta().hasEnchant(Enchantment.SILK_TOUCH)
-                    && player.hasPermission("epicspawners.silkdrop." + stack.getSpawnerData().getIdentifyingName())
+                    && player.hasPermission("epicspawners.silkdrop." + stack.getSpawnerData().getIdentifyingName().replace(' ','_'))
                     || player.hasPermission("epicspawners.no-silk-drop")) {
 
                 int ch = Integer.parseInt(instance.getConfig().getString((placedBy != null
