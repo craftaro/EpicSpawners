264ce7a39beae29c8304255464ef1cc52250be3e
Brianna <bri@songoda.com>
2019-04-14 16:10:41 -0400

    Fixed issue with random spawners including disabled spawners.

--- EpicSpawners-Plugin/src/main/java/com/songoda/epicspawners/spawners/spawner/ESpawnerManager.java
+++ EpicSpawners-Plugin/src/main/java/com/songoda/epicspawners/spawners/spawner/ESpawnerManager.java
@@ -59,7 +59,7 @@ public class ESpawnerManager implements SpawnerManager {
     @Override
     public Collection<SpawnerData> getAllEnabledSpawnerData() {
         Collection<SpawnerData> spawners = new ArrayList<>(getAllSpawnerData());
-        spawners.removeIf(spawnerData -> !spawnerData.isActive() && spawnerData.getIdentifyingName().equals("Omni"));
+        spawners.removeIf(spawnerData -> !spawnerData.isActive() || spawnerData.getIdentifyingName().equals("Omni"));
         return Collections.unmodifiableCollection(spawners);
     }
 
