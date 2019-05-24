85cb4513f004a278e62e21ee5c21618bfaee911f
Brianna <bri@songoda.com>
2019-05-24 01:44:58 -0400

    Added more categories.

--- src/main/java/com/songoda/epicspawners/utils/settings/Category.java
+++ src/main/java/com/songoda/epicspawners/utils/settings/Category.java
@@ -4,11 +4,13 @@ public enum Category {
 
     SPAWNER_BOOSTING("These are settings regarding the boosting of spawners."),
     SPAWNER_DROPS("These are settings regarding spawner drops."),
+    ENTITY("These are settings regarding entities."),
     MAIN("General settings and options."),
     MAIN_EQUATIONS("These are settings relating to equations."),
     INTERFACES("These settings allow you to alter the way interfaces look.",
             "They are used in GUI's to make patterns, change them up then open up a",
             "GUI to see how it works."),
+    DATABASE("Settings regarding the Database."),
     SYSTEM("System related settings.");
 
     private String[] comments;
