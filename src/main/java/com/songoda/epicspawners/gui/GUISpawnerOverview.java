de8a338c385156af95c4a7d987ab5fdd8be98d20
Brianna <bri@songoda.com>
2019-05-25 17:43:04 -0400

    Fixed occasional error with opening overview.

--- src/main/java/com/songoda/epicspawners/gui/GUISpawnerOverview.java
+++ src/main/java/com/songoda/epicspawners/gui/GUISpawnerOverview.java
@@ -20,6 +20,7 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -98,6 +99,8 @@ public class GUISpawnerOverview extends AbstractGUI {
         }
 
         List<Material> blocks = spawner.getFirstStack().getSpawnerData().getSpawnBlocksList();
+
+        if (blocks.isEmpty()) blocks = Collections.singletonList(Material.DIRT);
 
         StringBuilder only = new StringBuilder(blocks.get(0).name());
 
