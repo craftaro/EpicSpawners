fa25119b677ef37b0d810f6381a279c805b5447b
Brianna <bri@songoda.com>
2020-04-06 10:02:04 -0400

    Destroy spawners if they're empty.

--- src/main/java/com/songoda/epicspawners/tasks/SpawnerSpawnTask.java
+++ src/main/java/com/songoda/epicspawners/tasks/SpawnerSpawnTask.java
@@ -34,29 +34,33 @@ public class SpawnerSpawnTask extends BukkitRunnable {
     @Override
     public void run() {
         new ArrayList<>(manager.getSpawners()).forEach(spawner -> {
-            if (spawner.getWorld() == null
-                    || spawner.getWorld() == null
-                    || !spawner.getWorld().isChunkLoaded(spawner.getX() >> 4, spawner.getZ() >> 4)) return;
+            try {
+                if (spawner.getWorld() == null
+                        || spawner.getWorld() == null
+                        || !spawner.getWorld().isChunkLoaded(spawner.getX() >> 4, spawner.getZ() >> 4)) return;
 
-            if (spawner.getLocation().getBlock().getType() != CompatibleMaterial.SPAWNER.getMaterial()
-                    || spawner.getFirstStack() == null) {
-                spawner.destroy(plugin);
-                return;
+                if (spawner.getLocation().getBlock().getType() != CompatibleMaterial.SPAWNER.getMaterial()
+                        || spawner.getFirstStack() == null || spawner.getFirstStack().getSpawnerData() == null) {
+                    spawner.destroy(plugin);
+                    return;
+                }
+
+                if (spawner.getSpawnerDataCount() == 0
+                        || !spawner.checkConditions()
+                        || (spawner.getPlacedBy() == null && Settings.DISABLE_NATURAL_SPAWNERS.getBoolean())) return;
+
+                CreatureSpawner cSpawner = spawner.getCreatureSpawner();
+                if (cSpawner == null) return;
+                int delay = spawner.getCreatureSpawner().getDelay();
+                delay = delay - Settings.CUSTOM_SPAWNER_TICK_RATE.getInt();
+                spawner.getCreatureSpawner().setDelay(delay);
+                if (delay >= 0) return;
+
+                if (!spawner.spawn())
+                    spawner.updateDelay();
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-
-            if (spawner.getSpawnerDataCount() == 0
-                    || !spawner.checkConditions()
-                    || (spawner.getPlacedBy() == null && Settings.DISABLE_NATURAL_SPAWNERS.getBoolean())) return;
-
-            CreatureSpawner cSpawner = spawner.getCreatureSpawner();
-            if (cSpawner == null) return;
-            int delay = spawner.getCreatureSpawner().getDelay();
-            delay = delay - Settings.CUSTOM_SPAWNER_TICK_RATE.getInt();
-            spawner.getCreatureSpawner().setDelay(delay);
-            if (delay >= 0) return;
-
-            if (!spawner.spawn())
-                spawner.updateDelay();
         });
     }
 
