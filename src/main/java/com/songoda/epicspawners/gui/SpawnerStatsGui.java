fec8dc8628a5e6782f15f2b6f1fc39b395b9a319
Brianna <bri@songoda.com>
2021-02-07 09:48:53 -0600

    Fixed various issues with the spawnershop.

--- src/main/java/com/songoda/epicspawners/gui/SpawnerStatsGui.java
+++ src/main/java/com/songoda/epicspawners/gui/SpawnerStatsGui.java
@@ -12,6 +12,12 @@ import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -20,7 +26,7 @@ public class SpawnerStatsGui extends CustomizableGui {
 
     private final EpicSpawners plugin;
     private final Player player;
-    private int size = 0;
+    private final Map<SpawnerData, Integer> entities = new HashMap<>();
 
     public SpawnerStatsGui(EpicSpawners plugin, Player player) {
         super(plugin, "stats");
@@ -28,16 +34,19 @@ public class SpawnerStatsGui extends CustomizableGui {
         this.plugin = plugin;
         this.player = player;
 
-        for (Map.Entry<EntityType, Integer> entry : plugin.getPlayerDataManager().getPlayerData(player).getEntityKills().entrySet())
-            if (plugin.getSpawnerManager().getSpawnerData(entry.getKey()).isActive())
-                size++;
+        for (Map.Entry<EntityType, Integer> entry : plugin.getPlayerDataManager().getPlayerData(player).getEntityKills().entrySet()) {
+            SpawnerData data = plugin.getSpawnerManager().getSpawnerData(entry.getKey());
+            if (data.isActive())
+                entities.put(data, entry.getValue());
+        }
 
-        this.setTitle(plugin.getLocale().getMessage("interface.spawnerstats.title").getMessage());
-
+        setTitle(plugin.getLocale().getMessage("interface.spawnerstats.title").getMessage());
         showPage();
     }
 
     public void showPage() {
+        reset();
+
         // decorate the edges
         ItemStack glass2 = GuiUtils.getBorderItem(Settings.GLASS_TYPE_2.getMaterial(CompatibleMaterial.BLUE_STAINED_GLASS_PANE));
         ItemStack glass3 = GuiUtils.getBorderItem(Settings.GLASS_TYPE_3.getMaterial(CompatibleMaterial.LIGHT_BLUE_STAINED_GLASS_PANE));
@@ -51,26 +60,27 @@ public class SpawnerStatsGui extends CustomizableGui {
         mirrorFill("mirrorfill_4", 1, 0, true, true, glass2);
         mirrorFill("mirrorfill_5", 0, 1, true, true, glass2);
 
+        pages = (int) Math.max(1, Math.ceil(entities.size() / ((double) 28)));
+
         // enable page event
-        setNextPage(4, 7, GuiUtils.createButtonItem(CompatibleMaterial.ARROW, plugin.getLocale().getMessage("general.nametag.next").getMessage()));
-        setPrevPage(4, 1, GuiUtils.createButtonItem(CompatibleMaterial.ARROW, plugin.getLocale().getMessage("general.nametag.back").getMessage()));
+        setNextPage(5, 7, GuiUtils.createButtonItem(CompatibleMaterial.ARROW, plugin.getLocale().getMessage("general.nametag.next").getMessage()));
+        setPrevPage(5, 1, GuiUtils.createButtonItem(CompatibleMaterial.ARROW, plugin.getLocale().getMessage("general.nametag.back").getMessage()));
         setOnPage((event) -> showPage());
 
         setButton("exit", 8, GuiUtils.createButtonItem(CompatibleMaterial.valueOf(plugin.getConfig().getString("Interfaces.Exit Icon")),
                 plugin.getLocale().getMessage("general.nametag.exit").getMessage()), (event) -> player.closeInventory());
 
-        this.pages = (int) Math.floor(size / 28.0);
+        Set<Map.Entry<SpawnerData, Integer>> entries = entities.entrySet().stream().skip((page - 1) * 28).limit(28)
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())).collect(Collectors.toCollection(LinkedHashSet::new));
 
-        Set<Map.Entry<EntityType, Integer>> entries = plugin.getPlayerDataManager().getPlayerData(player).getEntityKills().entrySet();
-
-        entries = entries.stream().skip((page - 1) * 28).limit(28).collect(Collectors.toSet());
-
-        int num = 10;
-        for (Map.Entry<EntityType, Integer> entry : entries) {
+        int num = 11;
+        for (Map.Entry<SpawnerData, Integer> entry : entries) {
             if (num == 16 || num == 36)
                 num = num + 2;
+
             int goal = Settings.KILL_GOAL.getInt();
-            SpawnerData spawnerData = plugin.getSpawnerManager().getSpawnerData(entry.getKey());
+
+            SpawnerData spawnerData = entry.getKey();
 
             int customGoal = spawnerData.getKillGoal();
             if (customGoal != 0) goal = customGoal;
