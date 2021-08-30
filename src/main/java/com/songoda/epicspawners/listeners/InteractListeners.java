90fec80bb33bf6ec418622e19e1698284793ec8b
GKasparov <60962839+GKasparov@users.noreply.github.com>
2021-08-30 14:15:26 +1000

    giveOldEgg function
    This change is to add a function that will give the player the egg of the old mob of the spawner when they change the mob. It also adds an option in the config.yml to either enable or disable it.

--- src/main/java/com/songoda/epicspawners/listeners/InteractListeners.java
+++ src/main/java/com/songoda/epicspawners/listeners/InteractListeners.java
@@ -146,7 +146,14 @@ public class InteractListeners implements Listener {
             spawner.getCreatureSpawner().setSpawnedType(EntityType.DROPPED_ITEM);
         }
         spawner.getCreatureSpawner().update();
-
+        
+        if (plugin.getConfig().getBoolean("giveOldEgg")) {
+            CreatureSpawner creatureSpawner = (CreatureSpawner) block.getState();
+            ItemStack oldEgg = new ItemStack(item.getType(), 1);
+            ((SpawnEggMeta) oldEgg.getItemMeta()).setSpawnedType(creatureSpawner.getSpawnedType());
+            player.getInventory().addItem(oldEgg);
+        }
+        
         plugin.processChange(block);
         ItemUtils.takeActiveItem(player, CompatibleHand.getHand(event), bmulti);
     }
